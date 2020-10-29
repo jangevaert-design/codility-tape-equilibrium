2b4fdb8408bc2db68ec867b53797c5eac35e016e
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-29 16:35:58 -0600

    Initial commit

---
+++ src/TapeEquilibrium.java
@@ -0,0 +1,24 @@
+public class TapeEquilibrium {
+
+  public int solution (int[] A) {
+    int leftSum = A[0];
+    int rightSum = 0;
+    for (int i = 0; i < A.length -1; i++) {
+      rightSum += i;
+    }
+    rightSum -= leftSum;
+
+    int diff = Math.abs(leftSum - rightSum);
+
+    for (int i = 1; i < A.length - 1; i++) {
+      leftSum += A[i];
+      rightSum -= A[i];
+      int currentDiff = Math.abs(leftSum - rightSum);
+      if (diff > currentDiff) {
+        diff = currentDiff;
+      }
+;
+    }
+    return diff;
+  }
+}
